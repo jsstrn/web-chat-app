012812cca66c90b0a31254938e19acf011b19d5a
jsstrn <jessternrays@gmail.com>
2015-12-10 17:59:18 +0800

    Add new CSS layout

--- public/script.js
+++ public/script.js
@@ -1,5 +1,6 @@
 var socket = io()
 var username = window.prompt('Enter your username')
+var msg = document.querySelector('#msg')
 
 if (!username) {
   var ran = Math.floor((Math.random() * 999) + 100)
@@ -18,50 +19,60 @@ function getElapsedTime (timestamp) {
   return time
 }
 
-function addMessageSegment (message) {
+function addMessageToList (message) {
   var chat = document.querySelector('.chat')
   var section = document.createElement('section')
-  // var pUser = document.createElement('p')
-  // var pMsg = document.createElement('p')
-  // var pTime = document.createElement('p')
 
   var timestamp = new Date(message.timestamp)
   var time = getElapsedTime(timestamp)
 
-  section.innerHTML = ```
+  section.innerHTML = `
   <p>${message.username}</p>
-  <p class="msg">${message.message}</p>
-  <p>${time}</p>
-  ```
+  <p>${message.message}</p>
+  <p>${time}</p>`
+
   chat.appendChild(section)
+  section.scrollIntoView()
 }
 
-function addMessage (message) {
-  var messages = document.querySelector('#messages')
-  // var messages = document.getElementById('messages')
-  var li = document.createElement('li')
+// function checkMessage () {
+//   if (event.keyCode !== 13) return
+//   if (event.)
+// }
 
-  var timestamp = new Date(message.timestamp)
-  var time = getElapsedTime(timestamp)
-
-  li.textContent = `${message.username} ${time}: ${message.message}`
-  messages.appendChild(li)
-}
-
-$('form').submit(function () {
+function sendMessageToServer (event) {
+  console.log(event)
+  if (event.type === 'keydown' && event.keyCode !== 13) return
+  if (msg.value === '') return
   var timestamp = new Date()
-  var msg = {
+  var message = {
     username: username,
-    message: $('#msg').val(),
+    message: msg.value,
     timestamp: timestamp.toISOString()
   }
-  socket.emit('chat message', msg);
-  $('#msg').val('');
-  return false;
-});
+  socket.emit('chat message', message);
+  msg.value = ''
+}
 
-socket.on('chat message', addMessageSegment)
+var btn = document.querySelector('#btn')
+btn.addEventListener('click', sendMessageToServer, false)
+
+msg.addEventListener('keydown', sendMessageToServer, false)
+
+// $('form').submit(function () {
+//   var timestamp = new Date()
+//   var message = {
+//     username: username,
+//     message: msg.val(),
+//     timestamp: timestamp.toISOString()
+//   }
+//   socket.emit('chat message', message);
+//   msg.val('');
+//   return false;
+// });
+
+socket.on('chat message', addMessageToList)
 
 socket.on('chat log', function (messages) {
-  messages.forEach(addMessageSegment)
+  messages.forEach(addMessageToList)
 })
