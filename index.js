5c1bd468976f430b57e36e82fb6c659445f104c5
David Sng <davidsnghs@gmail.com>
2015-12-11 18:56:45 +0800

    Added social login
    Added social login

--- index.js
+++ index.js
@@ -3,24 +3,32 @@ const app = express()
 const http = require('http').Server(app)
 const io = require('socket.io')(http)
 const mongoose = require('mongoose')
+const socketioJwt = require('socketio-jwt')
 
 // database connection
 const db_user = process.env.CHAT_DB_USER || 'user'
 const db_pwd = process.env.CHAT_DB_PWD || 'password'
-const db_url =  'mongodb://' + db_user + ':' + db_pwd + '@ds027835.mongolab.com:27835/webchat'
+const db_url = 'mongodb://' + db_user + ':' + db_pwd + '@ds027835.mongolab.com:27835/webchat'
 mongoose.connect(db_url)
-const db = mongoose.connection
+// const db = mongoose.connection
 
 const Message = mongoose.model('Message', {
-  "username": String,
-  "message": String,
-  "timestamp": Object
+  username: String,
+  message: String,
+  timestamp: Object
 })
 
 app.use(express.static('public'))
 
+// enable auth0 log in
+
+io.use(socketioJwt.authorize({
+  secret: Buffer('Tqar8EmhhOMilDDeBEWXXjkn3__R2kksNVm5LmhxdzP97E89ZaLpwevEODkW_j2I', 'base64'),
+  handshake: true
+}))
+
 io.on('connection', (socket) => {
-  console.log('A user connected.')
+  console.log('A user connected.', socket.decoded_token.name)
   // send chat log on new user connection
   Message.model('Message').find((err, messages) => {
     if (err) return console.error(err)
